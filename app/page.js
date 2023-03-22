626e1747bd379feedc43c391148b9a353d6cb5e2
Kaleem Qasim <kaleemullahlqp@gmail.com>
2023-03-22 18:58:08 +0500

    tailwind typography

--- app/page.js
+++ app/page.js
@@ -7,9 +7,17 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className="text-3xl font-bold">
+      <h1 className="font-bold lg:text-sm selection:bg-yellow-300 text-green-900 border-4 border-green-700 hover:text-red-500 hover:border-red-300">
         Hello world!
       </h1>
+
+      <p className='text-xs text-red-600'>
+        testing xs
+      </p>
+
+      <p className='text-base text-red-600'>
+        testing base
+      </p>
     </main>
   )
 }
